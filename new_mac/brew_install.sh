ff64a7317a9e1a9a667db68e19dd80c64462d32d
thehappyboy <27151400@qq.com>
2024-01-20 23:21:16 +0800

    remove default input method

--- new_mac/brew_install.sh
+++ new_mac/brew_install.sh
@@ -64,3 +64,18 @@ brew tap homebrew/cask-fonts
 for font in "${fonts_list[@]}"; do
 	brew install --cask "$font"
 done
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
+done 2>/dev/null &
+
+# remove default input method
+cp ./com.apple.HIToolbox.plist ~/Library/Preferences/com.apple.HIToolbox.plist
+
+exit
