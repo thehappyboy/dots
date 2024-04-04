9e04c50033318b35ef00d5607fb65583c9b1d08d
thehappyboy <27151400@qq.com>
2024-04-04 17:37:19 +0800

    change python lsp to basedpyright, change spelllange exclude chinese

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@ vim.g.loaded_python_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.python3_host_prog = vim.fn.expand('/usr/local/Caskroom/miniconda/base/envs/nvim/bin/python3')
+
+vim.g.lazyvim_python_lsp = 'basedpyright'
+vim.o.spelllang = 'en_us,cjk'
