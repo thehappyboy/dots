7c6d6558ea3e870b3157615db1a30e5b6a049909
thehappyboy <27151400@qq.com>
2025-06-11 10:12:53 +0800

    add django config

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,17 +1,15 @@
 return {
-  -- lua with lazy.nvim
-  {
-    'max397574/better-escape.nvim',
-    event = 'VeryLazy',
-    config = function()
-      require('better_escape').setup()
-    end,
-  },
-
   {
     'linux-cultist/venv-selector.nvim',
     enabled = function()
       return LazyVim.has('telescope.nvim') or LazyVim.has('fzf-lua')
+    end,
+  },
+
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup()
     end,
   },
 }
