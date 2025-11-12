f733807d3c3786eeb421306aeace630641abb23b
thehappyboy <27151400@qq.com>
2025-11-12 15:26:09 +0800

    add rime input method config file

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,17 +1,4 @@
-local function has(plugin)
-  local plugins = require('lazy.core.config').spec.plugins
-  return plugins[plugin] ~= nil
-end
-
 return {
-  {
-    'linux-cultist/venv-selector.nvim',
-    enabled = function()
-      -- cond = function()
-      return has('fzf-lua') or has('telescope.nvim')
-    end,
-  },
-
   {
     'max397574/better-escape.nvim',
     config = function()
