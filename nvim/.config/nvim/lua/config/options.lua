bc637abee8c97a7843acf0b8a40473d76d8ebb6d
thehappyboy <27151400@qq.com>
2026-02-05 20:18:43 +0800

    using ty insted of basedpyright

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@ vim.g.python3_host_prog = vim.fn.expand('~/.config/nvim/.venv/bin/python')
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.g.lazyvim_python_lsp = 'basedpyright'
+vim.g.lazyvim_python_lsp = 'ty'
 vim.g.lazyvim_python_ruff = 'ruff'
