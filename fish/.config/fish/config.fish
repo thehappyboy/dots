dc1ec89d81e34e5092489eb66984d963cb180755
thehappyboy <27151400@qq.com>
2026-03-11 15:22:02 +0800

    add ghostty and starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -74,3 +74,7 @@ if status is-interactive
         starship init fish | source
     end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
