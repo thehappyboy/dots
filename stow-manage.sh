61462935daac8ed71dad0fd0a4835b0c8c7c636d
thehappyboy <27151400@qq.com>
2026-05-20 15:37:20 +0800

    add sublime settings

--- stow-manage.sh
+++ stow-manage.sh
@@ -3,5 +3,8 @@
 MODULES="npm uv bat fish git nvim ruff tmux warp zsh zed ghostty starship"
 
 for module in $MODULES; do
-  stow $module
+  stow "$module"
 done
+
+# Sublime Text - target 是 ~/Library/... 下的 Packages/User
+stow --target="$HOME/Library/Application Support/Sublime Text/Packages/User" sublime
